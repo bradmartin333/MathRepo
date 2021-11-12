835a5d8d29db7df0b4cc12716f5911f67a4fb814
BradDesktop <brad.martin@xdisplay.com>
2021-11-12 11:59:25 -0500

    add randomness

--- MathTests/UnitTests.cs
+++ MathTests/UnitTests.cs
@@ -1,68 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyMath;
-using System;
 
 namespace MathTests
 {
     [TestClass]
     public class UnitTests
     {
+        private readonly CoordArray coordArray = new CoordArray();
+
         [TestMethod]
         public void BruteForceCorners()
         {
-            CoordArray coordArray = new CoordArray();
-            (int, int)[] expectedResults = new (int, int)[] { 
-                (0, 0), 
-                (0, CoordArray.Max - 1), 
-                (CoordArray.Max - 1, 0), 
-                (CoordArray.Max - 1, CoordArray.Max - 1) };
-            foreach ((int, int) expectedResult in expectedResults)
-            {
-                (int, int) actualResult = coordArray.BruteForceFindIndex(expectedResult);
-                Assert.AreEqual(expectedResult, actualResult);
-            }
+            foreach ((int, int) expectedResult in CoordArray.Corners)
+                Assert.AreEqual(expectedResult, coordArray.BruteForceFindIndex(expectedResult));
         }
 
         [TestMethod]
-        public void BruteForceRandom()
+        public void BruteForceSingle()
         {
-            CoordArray coordArray = new CoordArray();
-            for (int i = 0; i < CoordArray.Max; i++)
-            {
-                Random r = new Random();
-                (int, int) expectedResult = (r.Next(0, CoordArray.Max), r.Next(0, CoordArray.Max));
-                (int, int) actualResult = coordArray.BruteForceFindIndex(expectedResult);
-                Assert.AreEqual(expectedResult, actualResult);
-            }
+            Assert.AreEqual(CoordArray.SingleIndex, coordArray.BruteForceFindIndex(CoordArray.SingleIndex));
         }
 
         [TestMethod]
         public void OptimizedCorners()
         {
-            CoordArray coordArray = new CoordArray();
-            (int, int)[] expectedResults = new (int, int)[] { 
-                (0, 0), 
-                (0, CoordArray.Max - 1), 
-                (CoordArray.Max - 1, 0), 
-                (CoordArray.Max - 1, CoordArray.Max - 1) };
-            foreach ((int, int) expectedResult in expectedResults)
-            {
-                (int, int) actualResult = coordArray.OptimizedFindIndex(expectedResult);
-                Assert.AreEqual(expectedResult, actualResult);
-            }
+            foreach ((int, int) expectedResult in CoordArray.Corners)
+                Assert.AreEqual(expectedResult, coordArray.OptimizedFindIndex(expectedResult));
         }
 
         [TestMethod]
-        public void OptimizedRandom()
+        public void OptimizedSingle()
         {
-            CoordArray coordArray = new CoordArray();
-            for (int i = 0; i < CoordArray.Max; i++)
-            {
-                Random r = new Random();
-                (int, int) expectedResult = (r.Next(0, CoordArray.Max), r.Next(0, CoordArray.Max));
-                (int, int) actualResult = coordArray.OptimizedFindIndex(expectedResult);
-                Assert.AreEqual(expectedResult, actualResult);
-            }
+            Assert.AreEqual(CoordArray.SingleIndex, coordArray.OptimizedFindIndex(CoordArray.SingleIndex));
         }
     }
 }
--- MyMath/Program.cs
+++ MyMath/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics;
+using System;
 
 namespace MyMath
 {
@@ -6,15 +7,30 @@ namespace MyMath
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             CoordArray coordArray = new CoordArray();
+            foreach ((int, int) expectedResult in CoordArray.Corners)
+                coordArray.OptimizedFindIndex(expectedResult);
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                CoordArray.SingleIndex = ((int)(CoordArray.Max * r.NextDouble()), (int)(CoordArray.Max * r.NextDouble()));
+                coordArray.OptimizedFindIndex(CoordArray.SingleIndex);
+            }
         }
     }
 
     public class CoordArray
     {
         public readonly static int Max = 1000;
-        private (int, int)[,] Arr = new (int, int)[Max, Max];
+        public static (int, int) SingleIndex = ((int)(Max * 0.1212), (int) (Max * 0.7231));
+        public readonly static (int, int)[] Corners = new (int, int)[] {
+                (0, 0),
+                (0, Max - 1),
+                (Max - 1, 0),
+                (Max - 1, Max - 1) };
+
+        private (double, double)[,] Arr = new (double, double)[Max, Max];
+
         public CoordArray()
         {
             int idx = 1;
@@ -22,33 +38,47 @@ namespace MyMath
             {
                 for (int j = 0; j < Max; j++)
                 {
-                    Arr[i, j] = (i, j);
+                    Random r = new Random();
+                    Arr[i, j] = (i + r.NextDouble(), j + r.NextDouble());
                     idx++;
                 }
             }
         }
 
-        public (int, int) BruteForceFindIndex((int, int) expectedResult)
+        public (int, int) BruteForceFindIndex((double, double) expectedResult)
         {
+            int counter = 0;
+            int x = -1;
+            int y = -1;
+            double delta = double.MaxValue;
             for (int i = 0; i < Max; i++)
             {
                 for (int j = 0; j < Max; j++)
                 {
-                    if (Arr[i, j] == expectedResult)
-                        return (i, j);
+                    double testDelta = Distance.Euclidean(
+                        new double[] { Math.Floor(Arr[i, j].Item1), Math.Floor(Arr[i, j].Item2) }, 
+                        new double[] { expectedResult.Item1, expectedResult.Item2});
+                    if (Math.Abs(testDelta) < delta)
+                    {
+                        x = i;
+                        y = j;
+                        delta = testDelta;
+                    }
+                    counter++;
                 }
             }
-            return (-1, -1);
+            return (x, y);
         }
 
-        public (int, int) OptimizedFindIndex((int, int) expectedResult)
+        public (int, int) OptimizedFindIndex((double, double) expectedResult)
         {
+            int counter = 0;
             int x = -1;
             int y = -1;
-            int delta = int.MaxValue;
+            double delta = double.MaxValue;
             for (int i = 0; i < Max; i++)
             {
-                int testDelta = Math.Abs(Arr[i, 0].Item1 - expectedResult.Item1);
+                double testDelta = Math.Abs(Math.Floor(Arr[i, 0].Item1) - expectedResult.Item1);
                 if (testDelta < delta)
                 {
                     x = i;
@@ -56,11 +86,12 @@ namespace MyMath
                 }
                 else
                     break;
+                counter++;
             }
             delta = int.MaxValue;
             for (int j = 0; j < Max; j++)
             {
-                int testDelta = Math.Abs(Arr[x, j].Item2 - expectedResult.Item2);
+                double testDelta = Math.Abs(Math.Floor(Arr[x, j].Item2) - expectedResult.Item2);
                 if (testDelta < delta)
                 {
                     y = j;
@@ -68,8 +99,10 @@ namespace MyMath
                 }
                 else
                     break;
+                counter++;
             }
-            return Arr[x, y];
+            Console.WriteLine($"{expectedResult,-10}\tOptimized Counter = {counter,5}\tPMax = {Math.Round(100.0 * ((double)counter / (Max * Max)), 2),5}%");
+            return (x, y);
         }
     }
 }
